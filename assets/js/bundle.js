5ec3626aa0d59d75723acc0bc949bb74d8e5c319
David Fawcett <david.fawcett@state.mn.us>
2014-10-03 13:19:02 -0500

    in bundle too

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var xhr = require('xhr');
 
-var map = new L.mapbox.Map('map-container', 'grafa.jdib780o', {
+var map = new L.mapbox.Map('map-container', {
         // remove the 'i'
         infoControl: false,
         // create a new attribution control
@@ -9,6 +9,10 @@ var map = new L.mapbox.Map('map-container', 'grafa.jdib780o', {
     });
 
 L.control.locate().addTo(map);
+
+var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+}).addTo(map);
 
 // Credit Foursquare for their wonderful data
 map.attributionControl
